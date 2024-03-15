3d8f4f5f49ca95a0dd49caa6a137671334a1add1
LariHericks <163542727+LariHericks@users.noreply.github.com>
2024-03-15 10:06:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    
+    document.getElementById("imagem").src="https://shop.mattel.com/cdn/shop/products/qgjefzecvqfdo6yesinu_e3dcff46-604e-4448-952a-c8a5755eeefc.jpg?v=1690236698"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src="https://i.pinimg.com/550x/a3/3d/7a/a33d7a4ac0f83c3ca17ba84be000dbf4.jpg"
+}
